13fcd1a14d1b8e662fe04ff8513df566029178f8
FarhodLatifov <latifovfarhod@gmail.com>
2026-02-25 07:53:01 +0500

    feat(layout): add viewport meta tag and title for improved SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,8 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Restaurant Massimo</title>
       </head>
       <body className={`${inter.className} overflow-x-hidden`}>
         <Providers>
